5c32bdb22eaf1b7c6d0d471d01a784af4bc0c707
harshadshirwadkar <harshadshirwadkar@gmail.com>
2011-06-29 14:00:50 +0530

    1) Added a module that creates a linked list of snapshots based on their creation time 2) Added a module that gathers all the changed blocks and revrets the filesystem to any snapshot
    TODO:
    1) Creating linked list on the basis of snapshot ID

--- ext4dev_restore.c
+++ ext4dev_restore.c
@@ -11,8 +11,85 @@
 #include <linux/fs.h>
 #include <linux/fiemap.h>
 
+#include <ftw.h>
+
 #define SNAPSHOT_SHIFT 4096*1036
 #define MAX 50
+#define MAX_FILE_NAME 1024
+
+struct snapshot_list {
+	char name[MAX_FILE_NAME];
+	unsigned long time;
+	struct snapshot_list *next;
+} *list_head;
+
+int add_to_snapshot_list(const char *fpath, const struct stat *sb, int type)
+{	
+	struct snapshot_list *list_node;
+
+	if(type != FTW_F)
+		return 0; 
+	
+	printf("\n%s %ld", fpath, sb->st_ctime);
+	if(list_head == NULL) {
+		list_node = (struct snapshot_list *)malloc(sizeof(struct snapshot_list));
+		strcpy(list_node->name, fpath);
+		list_node->time = sb->st_ctime;
+		list_node->next = NULL;
+		list_head = list_node;
+	}
+	else if (list_head->next == NULL) {
+		if(list_head->time < sb->st_ctime) {
+			list_node = (struct snapshot_list *)malloc(sizeof(struct snapshot_list));
+			strcpy(list_node->name, fpath);
+			list_node->time = sb->st_ctime;
+			list_node->next = NULL;
+
+			list_head->next = list_node;
+		}
+		else {
+			list_node = (struct snapshot_list *)malloc(sizeof(struct snapshot_list));
+			strcpy(list_node->name, fpath);
+			list_node->time = sb->st_ctime;
+
+			list_node->next = list_head;
+			list_head = list_node;
+		}
+	}
+	else {
+		if(sb->st_ctime < list_head->time) {
+			list_node = (struct snapshot_list *)malloc(sizeof(struct snapshot_list));
+			strcpy(list_node->name, fpath);
+			list_node->time = sb->st_ctime;
+			list_node->next = list_head;
+			list_head = list_node;			
+		}
+		else {
+			struct snapshot_list *temp;
+			list_node = list_head;
+			while(list_node->next && (list_node->next->time < sb->st_ctime))
+				list_node = list_node->next;
+			temp = (struct snapshot_list *)malloc(sizeof(struct snapshot_list));
+			strcpy(temp->name, fpath);
+			temp->time = sb->st_ctime;
+			temp->next = list_node->next;
+			list_node->next = temp;
+		}
+	}
+	return 0;
+}	
+
+int read_list(void)
+{
+	struct snapshot_list *node = list_head;
+
+	while(node != NULL) {
+		printf("\n%s", node->name);
+		node = node->next;
+	}
+
+	return 0;
+}
 
 struct fiemap *read_fiemap(int fd)
 {
@@ -38,6 +115,7 @@ struct fiemap *read_fiemap(int fd)
 
         extents_size = sizeof(struct fiemap_extent) * 
                               (fiemap->fm_mapped_extents);
+
 
         if ((fiemap = (struct fiemap*)realloc(fiemap,sizeof(struct fiemap) + 
                                          extents_size)) == NULL) {
@@ -78,60 +156,116 @@ void dump_fiemap(struct fiemap *fiemap, int snapshot_file, int disk_image)
 	}
 }
 
-void restore_fs(struct fiemap *fiemap, char *device_path, int disk_image)
+void restore_fs(char *device_path)
 {
 	int i;
-	int device=open("/dev/sda4", O_RDWR, 0660);
+	int device=open(device_path, O_RDWR, 0660), disk_image=open("/tmp/disk_image.img", O_RDONLY);
 	void *buf;
+	struct fiemap *fiemap;
+
+	fiemap = read_fiemap(disk_image);
 
 	for (i=0;i<fiemap->fm_mapped_extents;i++) {
-		if(fiemap->fm_extents[i].fe_logical - SNAPSHOT_SHIFT !=
+		if(fiemap->fm_extents[i].fe_logical !=
 		   fiemap->fm_extents[i].fe_physical){
 
 			buf = (void *)malloc(fiemap->fm_extents[i].fe_length);
 
-			lseek(disk_image, fiemap->fm_extents[i].fe_logical - SNAPSHOT_SHIFT, SEEK_SET);
+			lseek(disk_image, fiemap->fm_extents[i].fe_logical, SEEK_SET);
 			read(disk_image, buf, fiemap->fm_extents[i].fe_length);
 
-			lseek(device, fiemap->fm_extents[i].fe_logical - SNAPSHOT_SHIFT, SEEK_SET);
+			lseek(device, fiemap->fm_extents[i].fe_logical, SEEK_SET);
 			write(device, buf, fiemap->fm_extents[i].fe_length);
 
 			free(buf);
 		}
 	}
+	close(disk_image);
 	close(device);
+}
+
+void umount_and_exit(char *device, int status)
+{
+	umount(device);
+	rmdir("/mnt/temp");
+	exit(status);
 }
 
 int main(int argc, char **argv)
 {
         int fd, disk_image;
-	char command[MAX];
+	char command[MAX], mount_point[MAX], snapshot_dir[MAX], snapshot_file[MAX];
+	char *snapshot_name;
+	struct snapshot_list *node;
+	struct fiemap *fiemap;
+
+	list_head = NULL;
 
         if (argc != 3) {
-		printf("\next4dev_restore <device> <path_of_snapshot>\n");
+		printf("\next4dev_restore <device> <snapshot>\n");
                 exit(EXIT_FAILURE);
         }
 
+	if(mkdir("/mnt/temp",S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) != 0) {
+		printf("\nFailed to create mount point");
+		exit(EXIT_FAILURE);
+	}
+	strcpy(mount_point, "/mnt/temp");
+
+	sprintf(command,"snapshot.ext4dev mount %s %s",argv[1],mount_point);
+	system(command);
+
+	sprintf(snapshot_dir, "%s/.snapshots", mount_point);
+	sprintf(snapshot_file, "%s/%s", snapshot_dir, argv[2]);
+
+	if(ftw(snapshot_dir, add_to_snapshot_list, 10) != 0) {
+		printf("\nCannot walk throught fs");
+		umount_and_exit(argv[1], EXIT_FAILURE);
+	}
+
+	printf("\nFTW completed");
 	disk_image = open("/tmp/disk_image.img", O_CREAT | O_WRONLY, 0777);
 
-	if ((fd = open(argv[2], O_RDONLY)) < 0) {
-		fprintf(stderr, "Cannot open file %s\n", argv[2]);
+	if(list_head == NULL) {
+		printf("\nNo snapshot taken");
+		umount_and_exit(argv[1], EXIT_FAILURE);
 	}
-	else {
-		struct fiemap *fiemap;
 
-		if ((fiemap = read_fiemap(fd)) != NULL) 
-			dump_fiemap(fiemap, fd, disk_image);
-		close(fd);
-		close(disk_image);
-		disk_image = open("/tmp/disk_image.img", O_RDONLY);
+	node = list_head;
 
-		strcpy(command, "umount ");
-		strcat(command, argv[1]);
+	do {
+		snapshot_name = node->name + strlen(snapshot_dir) + 1;
+		sprintf(command, "snapshot.ext4dev enable %s", snapshot_name);
+		printf("\n%s\n",command);
 		system(command);
-
-		restore_fs(fiemap, argv[1], disk_image);
-	}
+		
+		if ((fd = open(node->name, O_RDONLY)) < 0) {
+			fprintf(stderr, "Cannot open file %s\n", node->name);
+			umount_and_exit(argv[1], EXIT_FAILURE);
+		}
+		else if ((fiemap = read_fiemap(fd)) != NULL) 
+			dump_fiemap(fiemap, fd, disk_image);
+		else {
+			printf("\nfiemap ioctl failed");
+			umount_and_exit(argv[1], EXIT_FAILURE);
+		}
+		close(fd);
+		node = node->next;
+		sprintf(command,"%s/%s",snapshot_dir,snapshot_name);
+		printf("com : %s \t file : %s\n",command,snapshot_file);
+	} while(strcmp(command, snapshot_file));
+	
 	close(disk_image);
+			
+	strcpy(command, "umount ");
+	strcat(command, mount_point);
+	system(command);
+
+	restore_fs(argv[1]);
+
+	sprintf(command, "fsck.ext4dev -fxy %s", argv[1]);
+	system(command);
+
+	rmdir("/mnt/temp");
         exit(EXIT_SUCCESS);
 }
